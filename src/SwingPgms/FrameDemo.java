d20811dd320fd5a965a75c062eed056cb7941b10
maddy <maddyraj65@gmail.com>
2016-06-15 20:12:38 +0530

    Demo of Java swing frame

---
+++ src/SwingPgms/FrameDemo.java
@@ -0,0 +1,14 @@
+package SwingPgms;
+import javax.swing.*;
+
+public class FrameDemo {
+
+	public static void main(String[] args) {
+		
+		JFrame jf = new JFrame();
+		jf.setSize(400,400);
+		jf.setTitle("Frame");
+		jf.setVisible(true);
+	}
+
+}
